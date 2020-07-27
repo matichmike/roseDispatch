f3e984cdd46d6229c3b33401c1c691c9a2ce1c11
Michael Matich <matich.mike@gmail.com>
2020-07-27 00:09:04 -0400

    schedule added with ability to add tasks/cleaned up code

--- src/App.js
+++ src/App.js
@@ -1,150 +1,239 @@
-import React, {useEffect, useState} from 'react';
-import './App.css';
-import TodoForm from "./components/TodoForm";
-import TodoList from "./components/TodoList";
-import Typography from "@material-ui/core/Typography";
-import {v4 as uuid} from "uuid";
-import buildYear from "./components/buildYear";
+import React, { useRef } from "react";
+import "./App.css";
+import { v4 as uuid } from "uuid";
+import { buildYear } from "./components/buildYear";
+import { observable, transaction } from 'mobx';
+import { observer } from "mobx-react";
 
-const LOCAL_STORAGE_KEY = "react-todo-list-todos";
-// {
+const DriverSelector = observer(({ viewModel }) => {
+  return (
+    <div>
+        <form>
+          <select value={viewModel.activeDriver} name="Drivers" id="Drivers" onChange={(e) => {
+              const value = e.target.value; 
+              viewModel.activeDriver = value;
+          }}>
+            <option value="Driver Selection">Driver Selection</option>
+            { viewModel.drivers.map((driver, i) => <option key={`${driver}-${i}`} value={driver}>{driver}</option> ) }
+          </select>
+        </form>
+      </div>
+  );
+});
 
-//   state: {
-//     drivers: [
-//       {'John':[{
-//         taskName: 'pickup', 
-//         date: new Date()}]},
-//       {'Ben':[{
-//         taskName: 'pickup', 
-//         date: new Date()}]},
-//       {'Ken':[{
-//         taskName: 'pickup', 
-//         date: new Date()}]}
-//     ]
-//   }
-// 
-//   setDrivers: () => { .... }
-// 
-// }
-
-
-
-class Task {
-  constructor(name, location, description, id) {
-    this.name = name;
-    this.location = location;
-    this.description = description;
-    this.id = id;
+const getDriverDay = (viewModel, day) => {
+  const { activeDriver, activeWeek, driverTimeline } = viewModel;
+  const currentDriverTimeline = driverTimeline[activeDriver];
+  if (!currentDriverTimeline) {
+    return;
   }
-
-  addTask(task) {
-    
-  }
+  return currentDriverTimeline[activeWeek].days[day];
 }
 
+const getTaskByDayTime = (viewModel, day, hour) => {
+  const { tasks } = viewModel;
+  const taskId = getDriverDay(viewModel, day).hours[hour];
+  const task = tasks.get(taskId);
+  return task;
+}
 
-function App() {
-  const initialTasks = [
-    {
-      1: {
-        driver: "John",
-        task: "Pick-up",
-        location: "Toronto",
-        description: "Refrigerators x10, Washing Machines x20"
+const DriverTimeTable = observer(({ viewModel }) =>{
+  
+  const rows = [];
+  for (let hour = 0; hour < 24; ++hour) {
+    const columns = [<td>{hour + 1}:00</td>];
+    for (let day = 0; day < 7; ++day) {
+        //.... todo: 
+      let task = getTaskByDayTime(viewModel, day, hour);  
+      if (task) {
+        task = <div>{task.type} - {task.description}</div>
       }
+      columns.push(<td>{task || 'No task'}</td>)
     }
-  ];
-
-  const initialDrivers = {
-    John: [],
-    Ben: [],
-    Ken: []
-  };
-
-  const [tasks, setTasks] = useState(initialTasks);
-
-  const [drivers, setDrivers] = useState(initialDrivers);
-
-  const initialYear = buildYear();
-
-  const [year, setYear] = useState(initialYear);
-  
-  
-
-  function addTask(driverName) {
-    const newTask = {taskname: 'dropoff', date: new Date()};
-    const driver = drivers[driverName];
-    const driverCopy = [...driver].push(newTask);
-    setDrivers({
-      ...drivers,
-      [driverName]: driverCopy
-    });
+    rows.push(<tr>
+      {columns}
+    </tr>)
   }
 
-  function handleClick(event) {
-    event.preventDefault();
-    addTask('John');
-  }
 
-  const [todos, setTodos] = useState([]);
-
-  useEffect(() => {
-    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storageTodos) {
-      setTodos(storageTodos);
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
-  }, [todos]);
-
-  function addTodo(todo) {
-    setTodos([todo, ...todos]);
-  }
-
-  function toggleComplete(id) {
-    setTodos(
-      todos.map(todo => {
-        if (todo.id === id) {
-          return {
-            ...todo,
-            completed: !todo.completed
-          };
-        }
-        return todo;
-      })
-    )
-  }
-
-  function removeTodo(id) {
-    setTodos(todos.filter(todo => todo.id !== id));
-  }
 
   return (
     <div>
-      <form>
-        <select name="Drivers" id="Drivers">
-        <option value="Driver Selection">Driver Selection</option>
-        <option value="John">John</option>
-        <option value="Ben">Ben</option>
-        <option value="Ken">Ken</option>
-        </select>
-        <button onClick={handleClick}>
-          add task
-        </button>
-      </form>
+      <table className="Schedule-table" style={{ border: 1 }}>
+        <tr>
+          <th></th>
+          <th>Monday</th>
+          <th>Tuesday</th>
+          <th>Wednesday</th>
+          <th>Thursday</th>
+          <th>Friday</th>
+          <th>Saturday</th>
+          <th>Sunday</th>
+        </tr>
+        {rows}
+      </table>
+    </div>
+  )
+})
+
+const hoursRangeSelect = [];
+for (let singleHour = 0; singleHour < 24; singleHour++) {
+  hoursRangeSelect.push(singleHour);
+}
+
+const WeekToggle = observer(({ viewModel }) => {
+  return (
+    <div>
+      <button onClick={() => {
+        if(viewModel.activeWeek<2) {
+          return;
+        }
+        viewModel.activeWeek--
+      }}>
+        Back
+      </button>
+      <label for="week">{viewModel.activeWeek}</label>
+      <button onClick={() => {
+        if(viewModel.activeWeek>51) {
+          return;
+        }
+        viewModel.activeWeek++
+      }}>
+        Forward
+      </button>
+    </div>
+    )
+})
+
+const AddDriverTask = ({ viewModel }) => {
+
+  const typeInput = React.useRef();
+  const locationInput = React.useRef();
+  const descriptionInput = React.useRef();
+  const daySelectInput = React.useRef();
+  const hourFromSelect = React.useRef();
+  const hourToSelect = React.useRef();
+
+  return (
+    <div>
+      <select ref={typeInput} name="Task Type">
+            <option>Select Task Type</option>
+            <option value="Pick-up">Pick-Up</option>
+            <option value="Drop-off">Drop-Off</option>
+            <option value="Other">Other</option>
+      </select>
+      <select ref={daySelectInput} name="Day Selection">
+            <option>Select Day</option>
+            <option value="0">Monday</option>
+            <option value="1">Tuesday</option>
+            <option value="2">Wednesday</option>
+            <option value="3">Thursday</option>
+            <option value="4">Friday</option>
+            <option value="5">Saturday</option>
+            <option value="6">Sunday</option>
+      </select>
+      <select ref={hourFromSelect} name="Hour From">
+            <option>From</option>
+            { hoursRangeSelect.map((i) => <option> {i}:00 </option> )}
+            
+            
+            
+      </select>
+      <select ref={hourToSelect} name="Hour To">
+            <option>To</option>
+            { hoursRangeSelect.map((i) => <option> {i}:00 </option> )}
+            
+      </select>
+      {/* todo: 1) day selector 2) hour selector I have problem with value?*/}
+      <input ref={locationInput} name="location" type="text" placeholder="Location" />
+      <input ref={descriptionInput} name="description" type="text" placeholder="Description" />
+      <button  onClick={() => {
+        const type = typeInput.current.value;
+        if (!type) {
+          return alert('Please select Type first');
+        }
+        const location = locationInput.current.value;
+        const description = descriptionInput.current.value;
+        const day = daySelectInput.current.value;
+        const from = Number.parseInt(hourFromSelect.current.value);
+        const to = Number.parseInt(hourToSelect.current.value);
+
+        // !!!! use Number.parseInt()
+        //  for day and hour selector
+        
+        // todo: assert TO < FROM !!!!!!
+        if (to < from ) {
+          return alert('Invalid time range!')
+        }
+
+
+
+         const driverDay = getDriverDay(viewModel, day);
+
+         // todo: check overlaps
+         // !!!!!
+         // !!!!!
+
+        const taskId = uuid();
+        const newTask = {
+          id: taskId,
+          type,
+          location,
+          description
+        };
+
+
+        transaction(() => {
+          viewModel.tasks.set(taskId, newTask);
+          
+          // if task is overriden completely - remove it from viewModel tasks
+
+          for (let i = from; i <= to; i++) {
+              driverDay.hours[i] = taskId;
+          }
+
+        });
+      }}>Add new task</button>
+    </div>
+  )
+}
+
+function App() {
+
+  const { current: viewModel } = useRef(observable({
+    drivers: ["John", "Ben", "Ken"],
+    tasks: new Map(),
+    activeDriver: "John",
+    driverTimeline: {
+      John: buildYear(),
+      Ben: buildYear(),
+      Ken: buildYear(),
+    },
+    activeWeek: 1
+  }));
+
+  return (
+    <div>
+      <p>Driver Schedule</p>
+      <DriverSelector viewModel={viewModel}></DriverSelector>
+
+      <div>
+        <p>Week toggle</p>
+        <WeekToggle viewModel={viewModel}></WeekToggle>
+      </div>
+
+      <div>
+        <AddDriverTask viewModel={viewModel}></AddDriverTask>
+      </div>
+
+      <div>
+        <p>Driver Timetable</p>
+        <DriverTimeTable viewModel={viewModel}></DriverTimeTable>
+      </div>
+
+      
     </div>
   );
 }
 
 export default App;
-
-{/* <div className="App">
-        <Typography style={{padding: 16}} variant="h1">rosedispatch</Typography>
-        <TodoForm addTodo={addTodo} />
-        <TodoList 
-        todos={todos} 
-        toggleComplete={toggleComplete} 
-        removeTodo={removeTodo}/>
-    </div> */}
--- src/components/buildYear.js
+++ src/components/buildYear.js
@@ -1,63 +1,19 @@
-class Day {
-  constructor() {
-    this.hours = new Array(24); // [_, _, _, task_id, task_id, task_id, ... _] undefined
-  }
-
-  getHour(index) {
-    return this.hours[index];
-  }
-
-  addTask(task, startHour, endHour) {
-    for (let i = startHour; i <= endHour; i++) {
-      this.hours[i] = task;
-    }
-  }
-}
-
-class Week {
-  constructor() {
-    this.days = [];
-    for (let i = 0; i < 7; i++) {
-      this.days.push(new Day());
-    }
-  }
-
-  getDay(index) { // 6
-    return this.days[index];
-  }
-}
-
-class Year {
-  constructor() {
-    this.weeks = [];
-    for (let i = 0; i < 52; i++) {
-      this.days.push(new Week());
-    }
-  }
-
-  getWeek(index) { // 10
-    return this.weeks[index];
-  }
-}
-
-function buildDay() {
-  return new Array(24);
-}
-
-function buildWeek() {
+const buildWeek = () => {
   const days = [];
-  for (let i = 0; i < 7; i++) {
-    days.push(buildDay());
+  for (let i = 0; i < 7; ++i) {
+    days.push({
+      hours: new Array(24)
+    })
   }
   return days;
 }
 
-function buildYear() {
+export const buildYear = () => {
   const weeks = [];
-  for (let i = 0; i < 52; i++) {
-    weeks.push(buildWeek());
+  for (let i = 0; i < 52; ++i) {
+    weeks.push({
+      days: buildWeek()
+    })
   }
   return weeks;
 }
-
-export default buildYear;
